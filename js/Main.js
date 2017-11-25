a668c804ccfd8e85e639be521e13fe1136b777ce
SAPIENT\asark4 <asarkar2@sapient.com>
2017-11-25 20:15:02 +0530

    dynamic flowchart with CSS3

--- js/Main.js
+++ js/Main.js
@@ -7,10 +7,14 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
+      jsonData: null
     };
   }
 
   componentDidMount() {
+    global.makeAjaxCall("/api/data.json", "GET").then((chartData) => {
+      this.setState({ jsonData: chartData })
+    })
   }
 
   componentWillMount() {
@@ -18,11 +22,60 @@ class Main extends Component {
   }
 
   render(props, state) {
+    if (!this.state.jsonData) {
+      return (
+        <div>Loading</div>
+      )
+    }
+    const jsonData = this.state.jsonData;
+    let itemsArrayOfObjects = Object.values(jsonData.items);
+    var streamsArrayOfObjects = Object.values(jsonData.streams);
+    console.log(itemsArrayOfObjects, streamsArrayOfObjects);
+    let blankLeft = 0;
+    let blankFlag = false;
     return (
       <div>
-        <CommonExample />
+        {streamsArrayOfObjects.map((stream, index) => {
+          blankLeft = 0;
+          blankFlag = true;
+          return (
+            <div key={index} className="streams-container">
+              <div className="stream-title">
+                {stream.title}
+              </div>
+              {
+                itemsArrayOfObjects.map((item, iIndex) => {
+                  if (item.stream === stream.id) {
+                    blankLeft++;
+                    {/* console.log(blankLeft); */ }
+                    let connectors = Object.values(item.connectors);
+                    console.log(connectors)
+                    return (
+                      <div key={iIndex} style={{ display: "inline-block" }}>
+                        {/* {<div className="node-wrapper"></div>} */}
+                        <div className="node-wrapper">
+                          <Nodes type={item.type} title={item.title} description={item.description} />
+                          {
+                            connectors.map((con, cIndex) => {
+                              return (
+                                <Connectors key={cIndex} type={con.type} />
+                              )
+                            })
+                          }
+                        </div>
+                      </div>
+                    )
+                  }
+                })
+              }
+
+            </div>
+          )
+        })}
+        {/* <Connectors type="dotted" /> */}
+        {/* <Nodes type="delay" title="Node 1 Title" description="description" /> */}
       </div>
-    );
+    )
   }
 }
 
--- js/commonComponents.js
+++ js/commonComponents.js
@@ -6,3 +6,19 @@ const CommonExample = ({ props }) => {
 		</div>
 	);
 };
+function Connectors(props) {
+  return (
+    <div className={`line-container ${props.type}`}>
+      <div className='arrow right'></div>
+      <div className='border-line'></div>
+    </div>
+  );
+}
+function Nodes(props) {
+  return (
+    <div className={`node-container ${props.type}`}>
+      <div className="node-title">{props.title}</div>
+      <div className="node-desc">{props.description}</div>
+    </div>
+  );
+}
